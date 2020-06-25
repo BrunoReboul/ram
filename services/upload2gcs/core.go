3e7bc4186e30dfaa8ac693db28d04793d5c5d117
BrunoReboul <brunoreboul@google.com>
2020-06-25 17:09:03 +0200

    upload2gcs ts

--- services/upload2gcs/core.go
+++ services/upload2gcs/core.go
@@ -119,11 +119,13 @@ func Initialize(ctx context.Context, global *Global) {
 		global.initFailed = true
 		return
 	}
+	log.Println("Done COLD START")
+
 }
 
 // EntryPoint is the function to be executed for each cloud function occurence
 func EntryPoint(ctxEvent context.Context, PubSubMessage ram.PubSubMessage, global *Global) error {
-	// log.Println(string(PubSubMessage.Data))
+	log.Println(string(PubSubMessage.Data))
 	if ok, _, err := ram.IntialRetryCheck(ctxEvent, global.initFailed, global.retryTimeOutSeconds); !ok {
 		return err
 	}
