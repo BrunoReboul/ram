c533d44648ffd8b86682f35b8a619741836ef896
BrunoReboul <brunoreboul@google.com>
2020-12-11 14:49:36 +0100

    feat:structured logs

--- services/stream2bq/core.go
+++ services/stream2bq/core.go
@@ -415,6 +415,8 @@ func EntryPoint(ctxEvent context.Context, PubSubMessage gps.PubSubMessage, globa
 		latency := now.Sub(metadata.Timestamp)
 		latencyE2E := now.Sub(originEventTimestamp)
 		parts := strings.Split(metadata.Resource.Name, "/")
+		stepID := fmt.Sprintf("%s/%s", parts[len(parts)-1], global.PubSubID)
+		fmt.Printf("%s || %s || %v ", metadata.Resource.Name, stepID, parts)
 		step := logging.Step{
 			StepID:        fmt.Sprintf("%s/%s", parts[len(parts)-1], global.PubSubID),
 			StepTimestamp: metadata.Timestamp,
